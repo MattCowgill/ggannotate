ebda8470bd27e7eae2990b2b26be70cf5ce3de2a
Matt Cowgill <mattcowgill@gmail.com>
2020-02-17 14:05:46 +1100

    test make_layer

--- tests/testthat/test-make_layer.R
+++ tests/testthat/test-make_layer.R
@@ -22,3 +22,19 @@ test_that("make_layer works with basic scatterplot", {
 test_that("annotated plot looks correct", {
   vdiffr::expect_doppelganger("annot_plot", annot_plot, path = NULL)
 })
+
+
+facet_plot_1 <- base_plot +
+  facet_wrap(~cyl)
+
+annot_facet_1 <- make_layer(geom = "text", x = 3, y = 30, label = "My\ntext",
+                            cyl = 4)
+
+annot_facet_1_plot <- facet_plot_1 +
+  eval(annot_facet_1)
+
+test_that("make_layer works with a single facet", {
+  expect_is(annot_facet_1, "call")
+  expect_is(annot_facet_1_plot, "ggplot")
+
+})
