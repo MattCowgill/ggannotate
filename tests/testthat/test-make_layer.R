6317e79b26e0e4598da2ecec14e2bf31a4397648
Matt Cowgill <mattcowgill@gmail.com>
2020-02-22 14:07:47 +1100

    working with input as code, as object name, or as RStudio selection (closes https://github.com/MattCowgill/ggannotate/issues/4)

--- tests/testthat/test-make_layer.R
+++ tests/testthat/test-make_layer.R
@@ -8,7 +8,11 @@ base_plot <- ggplot(mtcars,
 
 
 # Regular scatterplot ------
-annot <- make_layer(geom = "text", x = 3, y = 30, label = "My\ntext")
+annot <-
+  make_layer(geom = "text",
+             x = 3,
+             y = 30,
+             label = "My\ntext")
 annot_plot <- base_plot +
   eval(annot)
 
@@ -16,13 +20,20 @@ test_that("make_layer works with basic scatterplot", {
   expect_is(annot, "call")
   expect_is(annot_plot, "ggplot")
   vdiffr::expect_doppelganger("annot_plot", annot_plot, path = "make-layer")
-  })
+})
 
 # Faceted scatterplot -----
 facet_plot_1 <- base_plot +
-  facet_wrap(~cyl)
-annot_facet_1 <- make_layer(geom = "text", x = 3, y = 30, label = "My\ntext",
-                            facet_vars = list("cyl"), facet_levels = list(4))
+  facet_wrap( ~ cyl)
+annot_facet_1 <-
+  make_layer(
+    geom = "text",
+    x = 3,
+    y = 30,
+    label = "My\ntext",
+    facet_vars = list("cyl"),
+    facet_levels = list(4)
+  )
 annot_facet_1_plot <- facet_plot_1 +
   eval(annot_facet_1)
 
@@ -35,13 +46,19 @@ test_that("make_layer works with a single facet", {
 # Flipped bar chart -----
 flipped_bar_base <- mtcars %>%
   rownames_to_column("car") %>%
-  ggplot(aes(x = reorder(car, mpg),
-             y = mpg,
-             fill = factor(cyl))) +
+  ggplot(aes(
+    x = reorder(car, mpg),
+    y = mpg,
+    fill = factor(cyl)
+  )) +
   geom_col() +
   coord_flip()
 
-annot_flipped_bar <- make_layer(x = 2, y = 30, label = "My\nannotation", geom = "text")
+annot_flipped_bar <-
+  make_layer(x = 2,
+             y = 30,
+             label = "My\nannotation",
+             geom = "text")
 
 annot_flipped_bar_plot <- flipped_bar_base +
   eval(annot_flipped_bar)
@@ -57,8 +74,13 @@ date_line_base <- economics %>%
   ggplot(aes(x = date, y = unemploy)) +
   geom_line()
 
-annot_date_line <- make_layer(geom = "text", x = as.Date("1982-06-08"),
-                              y = 12781.0431364006, label = "My annotation")
+annot_date_line <-
+  make_layer(
+    geom = "text",
+    x = as.Date("1982-06-08"),
+    y = 12781.0431364006,
+    label = "My annotation"
+  )
 
 annot_date_line_plot <- date_line_base +
   eval(annot_date_line)
@@ -71,9 +93,13 @@ test_that("make_layer works with date axes", {
 
 # Curve ----
 
-annot_curve <- make_layer(geom = "curve",
-                          x = 2.20835965521693,
-                          y = 31.8618676116827, xend = 3.13939564982646, yend = 24.375414662927)
+annot_curve <- make_layer(
+  geom = "curve",
+  x = 2.20835965521693,
+  y = 31.8618676116827,
+  xend = 3.13939564982646,
+  yend = 24.375414662927
+)
 
 annot_curve_plot <- base_plot +
   eval(annot_curve)
