fda76ebfe64d13b4f976a0c54f3cbe8fac74edeb
Matt Cowgill <mattcowgill@gmail.com>
2020-07-31 17:14:11 +1000

    correctly handle facets that are factors; closes https://github.com/MattCowgill/ggannotate/issues/28

--- tests/testthat/test-facet_functions.R
+++ tests/testthat/test-facet_functions.R
@@ -63,34 +63,3 @@ base_plot <- p <- mtcars %>%
   facet_wrap(carb~factor(cyl))
 
 built_base_plot <- ggplot2::ggplot_build(base_plot)
-
-test_that("facet vars undisturbed when no brackets present", {
-
-  facets <- plot_facets(test_list)
-  vars <- strip_facet_brackets(facets, built_base_plot)
-  expect_identical(vars$vars, list(panelvar1 = "carb", panelvar2 = "cyl"))
-})
-
-test_that("brackets stripped from facet vars when present", {
-  new_test_list <- test_list
-  new_test_list$mapping$panelvar2 <- "factor(cyl)"
-
-  facets <- plot_facets(new_test_list)
-  vars <- strip_facet_brackets(facets, built_base_plot)
-  expect_identical(vars$vars, list(panelvar1 = "carb", panelvar2 = "cyl"))
-
-})
-
-
-test_that("strip_facet_brackets() doesn't modify vars if they can't be matched to data", {
-  wrong_test_list <- test_list
-  wrong_test_list$mapping$panelvar1 <- "factor(var_not_in_data)"
-
-
-  facets <- plot_facets(wrong_test_list)
-  vars <- strip_facet_brackets(facets, built_base_plot)
-  expect_identical(vars$vars, list(panelvar1 = "factor(var_not_in_data)", panelvar2 = "cyl"))
-})
-
-
-
