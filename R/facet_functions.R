23c1d1dd77665098ca65a790c1729a6762ec28d0
Matt Cowgill <mattcowgill@gmail.com>
2020-08-12 11:51:27 +1000

    new, faster, facets

--- R/facet_functions.R
+++ R/facet_functions.R
@@ -104,10 +104,11 @@ ggann_eval_facet <- function(facet, data) {
     }
     return(out)
   }
-  env <- rlang::new_environment(data)
-  mask <- rlang::new_data_mask(env)
-  mask$.data <- rlang::as_data_pronoun(mask)
-  tryCatch(rlang::eval_tidy(facet, mask), ggplot2_missing_facet_var = function(e) NULL)
+  # env <- rlang::new_environment(data)
+  # mask <- rlang::new_data_mask(env)
+  # mask$.data <- rlang::as_data_pronoun(mask)
+  # tryCatch(rlang::eval_tidy(facet, mask), ggplot2_missing_facet_var = function(e) NULL)
+  rlang::eval_tidy(facet, data)
 }
 
 #' This function is a modified copy of ggplot2:::eval_facets()
@@ -118,55 +119,52 @@ ggann_eval_facets <- function(facets, data) {
   dplyr::as_tibble(vars)
 }
 
-correct_facet_var <- function(facet_var, built_plot) {
-  stopifnot(inherits(built_plot, "ggplot_built"))
-
-  plot_data <- built_plot$plot$data
-
-  # If the string facet name can be uniquely matched to the colnames of the
-  # plot data, we don't need to do anything to the facet name
-  if (!is.null(plot_data[[facet_var]])) {
-    matched_facet_var <- facet_var
-
-  # If the facet name cannot be matched, evaluate the facet and then see if the
-  # data corresponding to the facet column can be matched to the plot data
-  } else {
-    facet_quos <- get_facet_quos(built_plot)
-    facet_quo <- facet_quos[[facet_var]]
-    facet_data_col <- ggann_eval_facet(facet_quo, plot_data)
-
-    matching_cols <- dplyr::select(plot_data,
-                            tidyselect::vars_select_helpers$where(function(x) all(x == facet_data_col)))
-
-    if (ncol(matching_cols) == 1) {
-      matched_facet_var <- names(matching_cols)
-    } else if (ncol(matching_cols) < 1) {
-      stop("facet var could not be matched to the plot data")
-    } else {
-      #stop("more than one column in the plot data matches the facet var")
-
-      var_between_brackets <- sub("^.*?\\((.*)\\)[^)]*$", "\\1", facet_var)
-
-
-      if (is.null(plot_data[[var_between_brackets]])) {
-        stop("facet variable ",facet_var,
-             " could not be found"," in the plot data")
-      }
-      matched_facet_var <- var_between_brackets
-    }
-  }
-  matched_facet_var
-}
-
-correct_facet_class <- function(facet_var, facet_level, plot_data) {
-
-  if (inherits(plot_data[[facet_var]], "factor")) {
-    call("factor", facet_level)
-  } else {
-    facet_level
-  }
-
-}
+# correct_facet_var <- function(facet_var, facet_quo, plot_data) {
+#
+#   # If the string facet name can be uniquely matched to the colnames of the
+#   # plot data, we don't need to do anything to the facet name
+#   if (!is.null(plot_data[[facet_var]])) {
+#     matched_facet_var <- facet_var
+#
+#   # If the facet name cannot be matched, evaluate the facet and then see if the
+#   # data corresponding to the facet column can be matched to the plot data
+#   } else {
+#     facet_data_col <- ggann_eval_facet(facet_quo, plot_data)
+#
+#     matching_cols <- dplyr::select(plot_data,
+#                             tidyselect::vars_select_helpers$where(function(x) all(x == facet_data_col)))
+#
+#     if (ncol(matching_cols) == 1) {
+#       matched_facet_var <- names(matching_cols)
+#     } else if (ncol(matching_cols) < 1) {
+#       stop("facet var could not be matched to the plot data")
+#     } else {
+#       #stop("more than one column in the plot data matches the facet var")
+#
+#       var_between_brackets <- sub("^.*?\\((.*)\\)[^)]*$", "\\1", facet_var)
+#
+#
+#       if (is.null(plot_data[[var_between_brackets]])) {
+#         stop("facet variable ",facet_var,
+#              " could not be found"," in the plot data")
+#       }
+#       matched_facet_var <- var_between_brackets
+#     }
+#   }
+#   matched_facet_var
+# }
+#
+# correct_facet_class <- function(facet_level, facet_quo, plot_data) {
+#
+#   facet_data <- ggann_eval_facet(facet_quo, plot_data)
+#
+#   if (inherits(facet_data, "factor")) {
+#     call("factor", facet_level)
+#   } else {
+#     facet_level
+#   }
+#
+# }
 
 
 get_facet_quos <- function(built_plot) {
@@ -178,5 +176,86 @@ get_facet_quos <- function(built_plot) {
   unlist(plot_facets)
 }
 
+# correct_facets_2 <- function(facets, built_plot) {
+#   facets_out <- facets
+#   facets_quos <- get_facet_quos(built_plot)
+#   plot_data <- built_plot$plot$data
+#
+#   for (facet_num in seq_along(facets_out$vars)) {
+#     facet_varname <- facets$vars[[facet_num]]
+#     facet_quo <- facets_quos[[facet_varname]]
+#
+#     facets_out$vars[[facet_num]] <- correct_facet_var(facets$vars[[facet_num]],
+#                                                       facet_quo,
+#                                                       plot_data)
+#
+#     facets_out$levels[[facet_num]] <- correct_facet_class(facets_out$levels[[facet_num]],
+#                                                           facet_quo,
+#                                                           plot_data)
+#   }
+#
+#   facets_out
+# }
+
+correct_facets_3 <- function(facets, built_plot) {
+  facets_out <- facets
+  facets_quos <- get_facet_quos(built_plot)
+  plot_data <- built_plot$plot$data
+
+  facets_data <- lapply(facets_quos,
+                        ggann_eval_facet,
+                        data = plot_data)
+
+  for (panelvar in names(facets$vars)) {
+
+    facet <- facets$vars[[panelvar]]
+    facet_data <- facets_data[[facet]]
+
+    # Attempt to get matching facet names (eg. 'cyl' rather than 'factor(cyl)')
+    # for facets that are not found in the colnames of the data
+    if (!facet %in% names(plot_data)) {
+      facets_out$vars[[panelvar]] <- match_facet_var(facet,
+                                                     facet_data,
+                                                     plot_data)
+    }
+
+    # Where facet is a factor, the level should be 'factor(x)' rather than 'x'
+    facet_factor <- inherits(facets_data[[facet]], "factor")
+    if (isTRUE(facet_factor)) {
+      facets_out$levels[[panelvar]] <- call("factor",
+                                            facets$levels[[panelvar]])
+    }
+  }
+
+  facets_out
+
+}
 
 
+match_facet_var <- function(facet, facet_data, plot_data) {
+
+    matching_cols <- purrr::map_lgl(plot_data, function(x) all(x == facet_data))
+
+    matching_cols <- matching_cols[matching_cols == TRUE]
+
+    if (length(matching_cols) == 1) {
+      # Unique match, return the name of the matching data column
+      matched_facet_var <- names(matching_cols)
+
+    } else if (length(matching_cols) < 1) {
+      stop("facet variable `", facet,
+           "` could not be found in the plot data")
+
+      # More than 1 match in the data, try and resolve if possible
+    } else {
+      var_between_brackets <- sub("^.*?\\((.*)\\)[^)]*$", "\\1", facet)
+
+      if (!var_between_brackets %in% names(matching_cols)) {
+        stop("facet variable `", facet,
+             "` could not be found in the plot data")
+      }
+      matched_facet_var <- var_between_brackets
+    }
+
+  matched_facet_var
+}
