fc9bec508cd92829bcacaae327be54d0ee67c6a7
Matt Cowgill <mattcowgill@gmail.com>
2020-08-12 12:20:27 +1000

    faster facet functions now functional

--- R/facet_functions.R
+++ R/facet_functions.R
@@ -1,6 +1,6 @@
-#' Given the list returned by Shiny as `input$plot_click`, get the
+#' Given the list returned by Shiny as `input$plot_click` and similar, get the
 #' facet variables (eg 'cyl') and levels (eg. '"4"') in the plot at the
-#' click location
+#' click location and return as a list
 #' @noRd
 
 get_facets <- function(plot_click) {
@@ -19,185 +19,19 @@ get_facets <- function(plot_click) {
   facets
 }
 
-#' The input$plot_click object from Shiny returns facet names as characters;
-#' this includes (eg.) "factor(cyl)" when the plot includes
-#' +facet_wrap(~factor(cyl)). We instead want to return "cyl", but where the
-#' faceted variable is a factor, we want the variable in the dataframe returned
+#' The `input$plot_click`` object (and similar) from Shiny returns
+#' facet names as characters; this includes (eg.) "factor(cyl)"
+#' when the plot includes +facet_wrap(~factor(cyl)).
+#' We instead want to return "cyl". Where the faceted variable is a factor,
+#' we want the variable in the dataframe returned
 #' by ggannoate to be a factor as well. This function takes the clicked facets
-#' (input$click modified by ggannotate::get_facets()) and the corresponding
-#' built plot and returns facets ready to be included in ggannotate's output
-#' @importFrom purrr compact
-#' @param clicked_facets list returned by get_facets()
-#' @param built_plot ggplot built with ggplot2::ggplot_build()
-correct_facets <- function(clicked_facets, built_plot) {
-  plot_facets <- list(built_plot$layout$facet_params$rows,
-                      built_plot$layout$facet_params$cols,
-                      built_plot$layout$facet_params$facets)
-
-  plot_facets <- purrr::compact(plot_facets)
-  plot_facets <- unlist(plot_facets)
-
-  plot_data <- built_plot$plot$data
-
-  facet_data <- ggann_eval_facets(plot_facets, plot_data)
-
-  matched_facets <- match_facet(facet_data, plot_data)
-
-  for (panelvar in names(clicked_facets$vars)) {
-    clicked_facet <- clicked_facets$vars[[panelvar]]
-
-    matches_click <- matched_facets$facet_name == clicked_facet
-
-    bare_var_name <- unlist(matched_facets$data_name[matches_click])
-    var_class <- unlist(matched_facets$class[matches_click])
-
-    clicked_facets$vars[[panelvar]] <- bare_var_name
-
-    if (var_class == "factor") {
-      clicked_facets$levels[[panelvar]] <- call("factor", clicked_facets$levels[[panelvar]])
-    }
-  }
-
-  clicked_facets
-}
-
+#' (eg. `input$click` modified by `ggannotate::get_facets()`) and the
+#' corresponding built plot and returns a list of facets ready to be
+#' included in ggannotate's output.
+#' @param facets list returned by `get_facets()`
+#' @param built_plot ggplot built with `ggplot2::ggplot_build()`
 #' @noRd
-match_facet <- function(facet_data, plot_data) {
-  matches <- list()
-
-  get_match <- function(facet_data_col, plot_data_col) {
-    if (all(facet_data[[facet_data_col]] == plot_data[[plot_data_col]])) {
-      c(matches,
-        list(facet_name = facet_data_col,
-             data_name = plot_data_col,
-             class = class(facet_data[[facet_data_col]])))
-    }
-  }
-
-  facet_data_cols <- colnames(facet_data)
-  plot_data_cols <- colnames(plot_data)
-
-  col_combinations <- expand.grid(facet_data_cols = facet_data_cols,
-                                  plot_data_cols = plot_data_cols,
-                                  stringsAsFactors = FALSE)
-
-  out <- purrr::map2(col_combinations$facet_data_cols,
-                     col_combinations$plot_data_cols,
-                     get_match)
-
-  out <- purrr::compact(out)
-  out <- purrr::transpose(out)
-  out
-}
-
-#' This function is a modified copy of ggplot2:::eval_facet()
-#' Copied here as that function is non-exported
-#' @noRd
-ggann_eval_facet <- function(facet, data) {
-  if (rlang::quo_is_symbol(facet)) {
-    facet <- as.character(rlang::quo_get_expr(facet))
-    if (facet %in% names(data)) {
-      out <- data[[facet]]
-    }
-    else {
-      out <- NULL
-    }
-    return(out)
-  }
-  # env <- rlang::new_environment(data)
-  # mask <- rlang::new_data_mask(env)
-  # mask$.data <- rlang::as_data_pronoun(mask)
-  # tryCatch(rlang::eval_tidy(facet, mask), ggplot2_missing_facet_var = function(e) NULL)
-  rlang::eval_tidy(facet, data)
-}
-
-#' This function is a modified copy of ggplot2:::eval_facets()
-#' Copied here as that function is non-exported
-#' @noRd
-ggann_eval_facets <- function(facets, data) {
-  vars <- purrr::compact(lapply(facets, ggann_eval_facet, data))
-  dplyr::as_tibble(vars)
-}
-
-# correct_facet_var <- function(facet_var, facet_quo, plot_data) {
-#
-#   # If the string facet name can be uniquely matched to the colnames of the
-#   # plot data, we don't need to do anything to the facet name
-#   if (!is.null(plot_data[[facet_var]])) {
-#     matched_facet_var <- facet_var
-#
-#   # If the facet name cannot be matched, evaluate the facet and then see if the
-#   # data corresponding to the facet column can be matched to the plot data
-#   } else {
-#     facet_data_col <- ggann_eval_facet(facet_quo, plot_data)
-#
-#     matching_cols <- dplyr::select(plot_data,
-#                             tidyselect::vars_select_helpers$where(function(x) all(x == facet_data_col)))
-#
-#     if (ncol(matching_cols) == 1) {
-#       matched_facet_var <- names(matching_cols)
-#     } else if (ncol(matching_cols) < 1) {
-#       stop("facet var could not be matched to the plot data")
-#     } else {
-#       #stop("more than one column in the plot data matches the facet var")
-#
-#       var_between_brackets <- sub("^.*?\\((.*)\\)[^)]*$", "\\1", facet_var)
-#
-#
-#       if (is.null(plot_data[[var_between_brackets]])) {
-#         stop("facet variable ",facet_var,
-#              " could not be found"," in the plot data")
-#       }
-#       matched_facet_var <- var_between_brackets
-#     }
-#   }
-#   matched_facet_var
-# }
-#
-# correct_facet_class <- function(facet_level, facet_quo, plot_data) {
-#
-#   facet_data <- ggann_eval_facet(facet_quo, plot_data)
-#
-#   if (inherits(facet_data, "factor")) {
-#     call("factor", facet_level)
-#   } else {
-#     facet_level
-#   }
-#
-# }
-
-
-get_facet_quos <- function(built_plot) {
-  plot_facets <- list(built_plot$layout$facet_params$rows,
-                      built_plot$layout$facet_params$cols,
-                      built_plot$layout$facet_params$facets)
-
-  plot_facets <- purrr::compact(plot_facets)
-  unlist(plot_facets)
-}
-
-# correct_facets_2 <- function(facets, built_plot) {
-#   facets_out <- facets
-#   facets_quos <- get_facet_quos(built_plot)
-#   plot_data <- built_plot$plot$data
-#
-#   for (facet_num in seq_along(facets_out$vars)) {
-#     facet_varname <- facets$vars[[facet_num]]
-#     facet_quo <- facets_quos[[facet_varname]]
-#
-#     facets_out$vars[[facet_num]] <- correct_facet_var(facets$vars[[facet_num]],
-#                                                       facet_quo,
-#                                                       plot_data)
-#
-#     facets_out$levels[[facet_num]] <- correct_facet_class(facets_out$levels[[facet_num]],
-#                                                           facet_quo,
-#                                                           plot_data)
-#   }
-#
-#   facets_out
-# }
-
-correct_facets_3 <- function(facets, built_plot) {
+correct_facets <- function(facets, built_plot) {
   facets_out <- facets
   facets_quos <- get_facet_quos(built_plot)
   plot_data <- built_plot$plot$data
@@ -232,8 +66,49 @@ correct_facets_3 <- function(facets, built_plot) {
 }
 
 
-match_facet_var <- function(facet, facet_data, plot_data) {
 
+#' This function is a simplified version of ggplot2:::eval_facet()
+#' Copied here as that function is non-exported
+#' @param facet A facet quosure, obtained through `get_facet_quos(built_plot)`
+#' @param data plot data, as in `build_plot$plot$data`
+#' @noRd
+ggann_eval_facet <- function(facet, data) {
+  if (rlang::quo_is_symbol(facet)) {
+    facet <- as.character(rlang::quo_get_expr(facet))
+    if (facet %in% names(data)) {
+      out <- data[[facet]]
+    }
+    else {
+      out <- NULL
+    }
+    return(out)
+  }
+
+  rlang::eval_tidy(facet, data)
+}
+
+
+#' Get list of facet quosures from built ggplot
+#' @noRd
+get_facet_quos <- function(built_plot) {
+  plot_facets <- list(built_plot$layout$facet_params$rows,
+                      built_plot$layout$facet_params$cols,
+                      built_plot$layout$facet_params$facets)
+
+  plot_facets <- purrr::compact(plot_facets)
+  unlist(plot_facets)
+}
+
+#' If facet name (eg. 'factor(cyl)'), try and find a unique match between the
+#' facet data and the data in the plot. If one is found, return it, if not
+#' error.
+#' @param facet facet as string, such as "cyl" or "factor(cyl)"
+#' @param facet_data vector; obtained by evaluating facet quosure in the plot
+#' environment using `ggann_eval_facet()`
+#' @param plot_data dataframe from built plot, `built_plot$plot$data`
+#' @noRd
+
+match_facet_var <- function(facet, facet_data, plot_data) {
     matching_cols <- purrr::map_lgl(plot_data, function(x) all(x == facet_data))
 
     matching_cols <- matching_cols[matching_cols == TRUE]
--- tests/testthat/test-facet_functions.R
+++ tests/testthat/test-facet_functions.R
@@ -172,81 +172,65 @@ test_that("factor in raw data for faceting recognised", {
 
 })
 
-### Test correct_facet_var ---
+test_that("factor with multiple matching columns works/fails as expected", {
+  plot_data <- mtcars
+  plot_data$cyl2 <- plot_data$cyl
+  plot_data$cyl <- factor(plot_data$cyl)
 
-test_that("correct_facet_var() handles normally-specified factors", {
-  regular_facet_plot <- test_base_plot +
-    facet_wrap(~cyl)
+  # Case where there are identical columns (cyl & cyl2) but one (cyl) is
+  # unambiguously specified in the facet call
+  facet_plot <- ggplot(plot_data, aes(x = wt, y = mpg)) + facet_grid(carb~cyl)
 
-  expect_identical(correct_facet_var("cyl", ggplot_build(regular_facet_plot)),
-                   "cyl")
+  test_list_facets <- test_base_list
+  test_list_facets$panelvar1 <- 6
+  test_list_facets$panelvar2 <- 3
+  test_list_facets$mapping$panelvar1 <- "carb"
+  test_list_facets$mapping$panelvar2 <- "cyl"
 
-  facet_grid_plot <- test_base_plot +
-    facet_grid(rows = "cyl")
+  facets <- get_facets(test_list_facets)
 
-  expect_identical(correct_facet_var("cyl", ggplot_build(facet_grid_plot)),
-                   "cyl")
+  expect_type(facets, "list")
+  expect_length(facets, 2)
+  expect_identical(facets$vars$panelvar2, "cyl")
+  expect_identical(facets$levels$panelvar2, 3)
+  expect_type(facets$levels$panelvar2, "double")
 
-  facet_grid_plot_2 <- test_base_plot +
-    facet_grid(cols = vars(cyl))
+  facets <- correct_facets(facets, ggplot2::ggplot_build(facet_plot))
 
-  expect_identical(correct_facet_var("cyl", ggplot_build(facet_grid_plot_2)),
-                   "cyl")
+  expect_type(facets, "list")
+  expect_length(facets, 2)
+  expect_identical(facets$vars$panelvar2, "cyl")
+  expect_type(facets$levels$panelvar2, "language")
+  expect_identical(eval(facets$levels$panelvar2), factor(3))
 
-})
+  # Case where there are identical columns and one is wrapped in 'factor()'
+  facet_plot <- ggplot(plot_data, aes(x = wt, y = mpg)) + facet_grid(carb~factor(cyl))
 
-test_that("correct_facet_var() deals with other facets", {
+  test_list_facets <- test_base_list
+  test_list_facets$panelvar1 <- 6
+  test_list_facets$panelvar2 <- 3
+  test_list_facets$mapping$panelvar1 <- "carb"
+  test_list_facets$mapping$panelvar2 <- "factor(cyl)"
 
-  factor_facet <- test_base_plot +
-    facet_wrap(~factor(cyl))
+  facets <- get_facets(test_list_facets)
+  facets <- correct_facets(facets, ggplot2::ggplot_build(facet_plot))
 
-  expect_identical(correct_facet_var("factor(cyl)", ggplot_build(factor_facet)),
-                   "cyl")
+  expect_identical(eval(facets$levels$panelvar2), factor(3))
+  expect_identical(facets$vars$panelvar2, "cyl")
 
-  mtcars_2 <- mtcars
-  mtcars_2$cyl2 <- mtcars$cyl
-
-  double_var_plot <- ggplot(mtcars_2, aes(x = wt, y = mpg)) +
-    geom_point() +
-    facet_wrap(~cyl)
-
-  expect_identical(correct_facet_var("cyl", ggplot_build(double_var_plot)),
-                   "cyl")
-
-  double_var_plot <- ggplot(mtcars_2, aes(x = wt, y = mpg)) +
-    geom_point() +
-    facet_wrap(~factor(cyl))
-
-  expect_identical(correct_facet_var("factor(cyl)", ggplot_build(double_var_plot)),
-                   "cyl")
-
-  double_var_plot <- ggplot(mtcars_2, aes(x = wt, y = mpg)) +
-    geom_point() +
-    facet_wrap(~factor(cyl, levels = c("8", "6", "4")))
-
-  expect_error(correct_facet_var('factor(cyl, levels = c("8", "6", "4")',
-                                     ggplot_build(double_var_plot)))
-
-})
-
-test_that("correct_facet_class() returns a call to factor() when var is factor", {
-  mtcars2 <- mtcars
-  mtcars2$cyl2 <- factor(mtcars$cyl)
-
-  expect_type(correct_facet_class("cyl", 6, mtcars2),
-              "double")
-
-  expect_identical(
-    correct_facet_class("cyl", 6, mtcars2),
-    6
-  )
-
-  expect_type(correct_facet_class("cyl2", 6, mtcars2),
-              "language")
+  # Case where there are identical columns and one is wrapped in a longer
+  # function call
+  facet_plot <- ggplot(plot_data, aes(x = wt, y = mpg)) + facet_grid(carb~factor(cyl, levels = c("8", "6", "4")))
 
 
-  expect_identical(correct_facet_class("cyl2", 6, mtcars2),
-                   call("factor", 6))
+  test_list_facets <- test_base_list
+  test_list_facets$panelvar1 <- 6
+  test_list_facets$panelvar2 <- 3
+  test_list_facets$mapping$panelvar1 <- "carb"
+  test_list_facets$mapping$panelvar2 <- 'factor(cyl, levels = c("8", "6", "4")'
 
+  facets <- get_facets(test_list_facets)
+  # This should fail
+  expect_error(correct_facets(facets, ggplot2::ggplot_build(facet_plot)))
 
 })
